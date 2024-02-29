450ccd4dd2d4b9d45779af9c30f531c965f2d1e5
aaroj <aaro.jylhamaki@outlook.com>
2024-02-29 20:32:21 +0200

    added parcel/destination combiner function. added choose_a_parcel function. added choose_an_airplane function.

---
+++ game/Player_turn_choices.py
@@ -0,0 +1,93 @@
+from geopy import distance
+#test_player_data = {"playername": "abc", "co2_produced": 0, "location": (60.3172, 24.963301), "player_parcels": [{'item': 'vasara', 'co2_item': 200, 'heft': 8.19, 'info': 'Vasara on hieno vasara', 'destination_airport': 'Aavahelukka Airport', 'destination_country': 'Suomi', 'latitude': 67.60359954833984, 'longitude': 23.97170066833496},
+# {'item': 'omena', 'co2_item': 300, 'heft': 0.35, 'info': 'Omena on hieno omena', 'destination_airport': 'Ahmosuo Airport', 'destination_country': 'Suomi', 'latitude': 64.895302, 'longitude': 25.752199},
+# {'item': 'tietokone', 'co2_item': 150, 'heft': 2.7, 'info': 'tietoinen kone', 'destination_airport': 'Alavus Airfield', 'destination_country': 'Suomi', 'latitude': 62.554699, 'longitude': 23.573299},
+# {'item': 'possu', 'co2_item': 175, 'heft': 0.76, 'info': 'röh röh', 'destination_airport': 'Jorvin Hospital Heliport', 'destination_country': 'Suomi', 'latitude': 60.220833, 'longitude': 24.68639},
+# {'item': 'kaakao', 'co2_item': 500, 'heft': 2.78, 'info': 'makeaa', 'destination_airport': 'Kilpisjärvi Heliport', 'destination_country': 'Suomi', 'latitude': 69.0022201538086, 'longitude': 20.89638900756836},
+#], "player_delivered": [{'item': 'vasara', 'co2_item': 200, 'heft': 8.19, 'info': 'Vasara on hieno vasara', 'destination_airport': 'Aavahelukka Airport', 'destination_country': 'Suomi', 'latitude': 67.60359954833984, 'longitude': 23.97170066833496},
+# {'item': 'omena', 'co2_item': 300, 'heft': 0.35, 'info': 'Omena on hieno omena', 'destination_airport': 'Ahmosuo Airport', 'destination_country': 'Suomi', 'latitude': 64.895302, 'longitude': 25.752199},
+# {'item': 'kaakao', 'co2_item': 500, 'heft': 2.78, 'info': 'makeaa', 'destination_airport': 'Kilpisjärvi Heliport', 'destination_country': 'Suomi', 'latitude': 69.0022201538086, 'longitude': 20.89638900756836},
+#] }
+test_player_data = {"playername": "abc", "co2_produced": 0, "location": (60.3172, 24.963301), "player_parcels": [{'item': 'vasara', 'co2_item': 200, 'heft': 8.19, 'info': 'Vasara on hieno vasara', 'destination_airport': 'Aavahelukka Airport', 'destination_country': 'Suomi', 'latitude': 67.60359954833984, 'longitude': 23.97170066833496},
+ {'item': 'omena', 'co2_item': 300, 'heft': 0.35, 'info': 'Omena on hieno omena', 'destination_airport': 'Ahmosuo Airport', 'destination_country': 'Suomi', 'latitude': 64.895302, 'longitude': 25.752199},
+ {'item': 'tietokone', 'co2_item': 150, 'heft': 2.7, 'info': 'tietoinen kone', 'destination_airport': 'Alavus Airfield', 'destination_country': 'Suomi', 'latitude': 62.554699, 'longitude': 23.573299},
+ {'item': 'possu', 'co2_item': 175, 'heft': 0.76, 'info': 'röh röh', 'destination_airport': 'Jorvin Hospital Heliport', 'destination_country': 'Suomi', 'latitude': 60.220833, 'longitude': 24.68639},
+ {'item': 'kaakao', 'co2_item': 500, 'heft': 2.78, 'info': 'makeaa', 'destination_airport': 'Kilpisjärvi Heliport', 'destination_country': 'Suomi', 'latitude': 69.0022201538086, 'longitude': 20.89638900756836},
+], "player_delivered": [] }
+
+def choose_a_parcel(player_data):
+    """Takes the acting players data as input and lets the player choose a parcel to deliver.
+    Returns the index of the parcel in the list: 'player_parcels' in player data."""
+
+    # prints parcel options to the player
+    for parcel in player_data.get("player_parcels"):
+        parcel_number = player_data.get("player_parcels").index(parcel)+1
+
+        if parcel in player_data.get("player_delivered"):
+            parcel_number = "X"
+
+        distance_from_player = distance.distance(player_data.get("location"), (parcel.get("latitude"), parcel.get("longitude")))
+
+        print(f"[{parcel_number}]: Tuote: {parcel.get("item")}, Paino: {parcel.get("heft")} kg, Kohde: {parcel.get("destination_airport")}, Etäisyys: {float(str(distance_from_player)[:-3]):.2f} km")
+
+    # creates a list of valid inputs to be used later to check if the players input is valid
+    list_of_valid_inputs = []
+    for parcel in player_data.get("player_parcels"):
+        if parcel not in player_data.get("player_delivered"):
+            list_of_valid_inputs.append(str(player_data.get("player_parcels").index(parcel)+1))
+
+    # asks the player to give an input and doesn't let the player proceed until a valid input is given
+    while True:
+
+        player_input = input("Valitse kuljetettava paketti: ")
+
+        if player_input in list_of_valid_inputs:
+            print(f"Valitsit paketin {player_input}")
+            break
+        elif player_input in ["1","2","3","4","5"]:
+            print(f"Olet jo kuljettanut tämän paketin")
+        else:
+            print("Tämä ei ole validi syöte")
+
+    return str(int(player_input)-1)
+
+
+
+def choose_airplane(chosen_location, player_data):
+    """Takes the chosen travel location and the data of the acting player,
+    lets the player choose an airplane and returns the number corresponding to the chosen airplane.
+    The numbers that correspond to the airplanes are:
+    1. "Rahtikone"
+    2. "Matkustajakone"
+    3. "Yksityiskone" """
+
+    #calculates the distance from players current location to the chosen destination
+    distance_from_player = distance.distance(player_data.get("location"), ((player_data.get("player_parcels")[int(chosen_location)-1].get("latitude")), (player_data.get("player_parcels")[int(chosen_location)-1].get("longitude"))))
+
+    #calculates the default flight time that is used to calculate the flight times of different airplane options
+    default_flight_time = float(str(distance_from_player)[:-3]) / 800 # flight time in hours, 800km/h used as a default speed of a commercial airplane
+
+    #prints the airplane options with flight_times for the player to see
+    print(f"Valitsemasi kohde on {player_data.get("player_parcels")[int(chosen_location)].get("destination_airport")}, etäisyys {float(str(distance_from_player)[:-3]):.2f} km")
+    print(f"[1]: Rahtilentokoneella kuljetus kestää {default_flight_time*1.5:.1f} tuntia")
+    print(f"[2]: Matkustajakoneella kuljetus kestää {default_flight_time:.1f} tuntia")
+    print(f"[3]: Yksityiskoneella kuljetus kestää {default_flight_time*0.75:.1f} tuntia")
+
+    #asks the player to choose an airplane and doesn't let then proceed until a valid input is given
+    while True:
+
+        player_input = input("Valitse lentokone: ")
+
+        if player_input in ["1","2","3"]:
+            print(f"Valitsit lentokoneen {player_input}")
+            break
+        else:
+            print("Tämä ei ole validi syöte")
+
+    return player_input
+
+
+chosen_parcel_index = choose_a_parcel(test_player_data)
+chosen_airplane = choose_airplane(chosen_parcel_index, test_player_data)
+
+print(chosen_airplane, chosen_parcel_index)
